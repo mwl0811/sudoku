430d086890ee6947642b7e4ba506a37f7d09b3bd
admin <admin@DESKTOP-J4SKA27>
2018-12-19 17:01:53 +0800

    更改部分信息

--- sudoku/UnitTest1/unittest1.cpp
+++ sudoku/UnitTest1/unittest1.cpp
@@ -1,11 +1,12 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include"../sudoku/Generator.h"
-#include"../sudoku/Solver.h"
-#include"../sudoku/sudoku.cpp"
-#include<cassert>
-#include<cstring>
-#include<set>
+#include "../sudoku/Generator.h"
+#include "../sudoku/Solver.h"
+#include "../sudoku/sudoku.cpp"
+#include <windows.h>
+#include <cassert>
+#include <cstring>
+#include <set>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -15,31 +16,166 @@ namespace UnitTest_Sudoku
 	{
 	public:
 		int argc;
-		char **cargv, **sargv;
+		char **argv;
 
 		UnitTest1()
 		{
-			argc = 3;
-			cargv = new char*[3];
-			sargv = new char*[3];   //命令由三部分组成
+			argv = new char*[3];
 
-			cargv[0] = new char[100];
-			sargv[0] = new char[100];
-			strcpy_s(cargv[0], 100, "sudoku.exe");
-			strcpy_s(sargv[0], 100, "sudoku.exe");
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
 
-			cargv[1] = new char[100];
-			sargv[1] = new char[100];
-			strcpy_s(cargv[1], 100, "-c");
-			strcpy_s(sargv[1], 100, "-s");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-c");
 
-			cargv[2] = new char[100];
-			sargv[2] = new char[100];
-			strcpy_s(cargv[2], 100, "100");
-			strcpy_s(sargv[2], 100, "solver.txt");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "100");
 		}
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestMethod1)   //输入格式不正确的时候
+		{
+			argc = 1;
+			Check check1(argc, argv);
+			int result = check1.checkinput();
+			assert(result == 1);
+		}
+
+		TEST_METHOD(TestMethod2)   //输入的不是-c或者-s的时候
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-k");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "100");
+
+			Check check2(argc, argv);
+			int result = check2.checkinput();
+			assert(result == 2);
+		}
+
+		TEST_METHOD(TestMethod3)   //输入不是数字的时候
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-c");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "abc");
+
+			Check check3(argc, argv);
+			int result = check3.checkinput();
+			assert(result == 3);
+		}
+
+		TEST_METHOD(TestMethod4)   //输入的数字过大
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-c");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "1000009");
+
+			Check check4(argc, argv);
+			int result = check4.checkinput();
+			assert(result == 4);
+		}
+
+		TEST_METHOD(TestMethod5)   //输入生成数独终盘命令正确
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-c");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "10000");
+
+			Check check5(argc, argv);
+			int result = check5.checkinput();
+			assert(result == 5);
+		}
+
+		TEST_METHOD(TestMethod6)   //解数独题的路径错误
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-s");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "100");
+
+			Check check6(argc, argv);
+			int result = check6.checkinput();
+			assert(result == 6);
+		}
+
+		TEST_METHOD(TestMethod7)   //解数独题的路径正确
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-s");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "solver.txt");
+
+			Check check7(argc, argv);
+			int result = check7.checkinput();
+			assert(result == 7);
+		}
+
+		TEST_METHOD(TestMethod8)   //输入算式
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-c");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "10/5");
+
+			Check check8(argc, argv);
+			int result = check8.checkinput();
+			assert(result == 8);
+		}
+
+		TEST_METHOD(TestMethod9)
+		{
+			argc = 3;
+
+			argv = new char*[3];
+			argv[0] = new char[100];
+			strcpy_s(argv[0], 100, "sudoku.exe");
+			argv[1] = new char[100];
+			strcpy_s(argv[1], 100, "-s");
+			argv[2] = new char[100];
+			strcpy_s(argv[2], 100, "solver.txt");
+
+			Check check7(argc, argv);
+			assert(check7.flag == 0);
+		}
+
+		TEST_METHOD(TestMethod10)   //证明Generator没有生成重复的矩阵
 		{
 			int sudoku_number = 1000000;
 			FILE* file;
@@ -58,7 +194,7 @@ namespace UnitTest_Sudoku
 			while (true)
 			{
 				int temp;
-				for (int i = 0; i < 9; i++) 
+				for (int i = 0; i < 9; i++)
 				{
 					for (int j = 0; j < 9; j++)
 					{
@@ -78,12 +214,5 @@ namespace UnitTest_Sudoku
 			fclose(stdin);
 			assert(container.size() != sudoku_number);
 		}
-
-		TEST_METHOD(TestMethod2)
-		{
-			
-		}
-
-
 	};
 }
--- sudoku/sudoku/Solver.h
+++ sudoku/sudoku/Solver.h
@@ -10,20 +10,28 @@
 #include <iostream>
 using namespace std;
 
+int flag = 0;
 class Solver{
 public:
 	int sudoku[9][9];
 	FILE* question;
 	FILE* file_write;
+	int flag;
 	
 	Solver(FILE* path,FILE* ans)
 	{
+		flag = 0;
 		question = path;
 		file_write = ans;
 		memset(sudoku,0,sizeof(sudoku));
 	}
 
-	void in()
+	int test()
+	{
+		return flag;
+	}
+
+	int in()
 	{
 		while (true)
 		{
@@ -43,9 +51,12 @@ public:
 			
 			if (dfs(0))
 			{
+				if (valid(sudoku) != 1)
+					flag = 1;
 				Out();    //解决之后输出
 			}
 		}
+		return test();
 	}
 
 private:
@@ -130,6 +141,63 @@ private:
 			for (int j = 0; j < 3; j++)
 			{
 				if ((line != base_line + i) && (col != base_col + j) && sudoku[base_line + i][base_col + j] == num)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	bool valid(int sudoku[][9]) 
+	{
+		for (int i = 0; i < 9; i++) 
+		{
+			bool line_exist[10];
+			memset(line_exist, 0, sizeof(line_exist));
+			for (int j = 0; j < 9; j++) {
+				if ((i == 0 && (j == 0 || j == 3 || j == 6)) || (i == 3 && (j == 0 || j == 3 || j == 6))
+					|| (i == 6 && (j == 0 || j == 3 || j == 6))) 
+				{
+					bool exist[10];
+					memset(exist, 0, sizeof(exist));
+					for (int cell_i = 0; cell_i < 3; cell_i++)
+					{
+						for (int cell_j = 0; cell_j < 3; cell_j++) 
+						{
+							exist[sudoku[cell_i + i][cell_j + j]] = true;
+						}
+					}
+					for (int exist_i = 1; exist_i < 10; exist_i++)
+					{
+						if (!exist[exist_i])
+							return false;
+					}
+				}
+
+				line_exist[sudoku[i][j]] = true;
+			}
+
+			for (int j = 1; j <= 9; j++)
+			{
+				if (!line_exist[j])
+				{
+					return false;
+				}
+			}
+		}
+
+		for (int i = 0; i < 9; i++)
+		{
+			bool col_exist[10];
+			memset(col_exist, 0, sizeof(col_exist));
+			for (int j = 0; j < 9; j++)
+			{
+				col_exist[sudoku[j][i]] = true;
+			}
+			for (int j = 1; j <= 9; j++)
+			{
+				if (!col_exist[j]) 
 				{
 					return false;
 				}
